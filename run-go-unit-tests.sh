77e26774a92bf40ccd56fd66d6d5142e6a451e6a
cabuda <cabuda@outlook.com>
2023-07-14 16:17:28 +0800

    Update run-go-unit-tests.sh

--- run-go-unit-tests.sh
+++ run-go-unit-tests.sh
@@ -7,4 +7,9 @@ fail() {
 
 # FILES=$(go list ./... | grep -v /vendor/) || fail
 # go test -timeout 30s -short -v ${FILES} || fail
-go test -timeout 30s -short -v ./... || fail
+
+for file in $(find . -name "go.mod"); do
+    dir=$(dirname "$file")
+    cd "$dir"
+    go test -timeout 30s -short -v ./... || fail
+done
