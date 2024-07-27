d0260accad5dd37caa3ac76b87ba0a0c37a5cc65
cabuda <cabuda@outlook.com>
2024-07-27 10:39:21 +0800

    Update run-go-unit-tests.sh

--- run-go-unit-tests.sh
+++ run-go-unit-tests.sh
@@ -5,15 +5,15 @@ fail() {
   exit 1
 }
 
-# 查找所有包含go.mod的子目录
-directories=$(find . -name "go.mod" -exec dirname {} \;)
+# 步骤 1：获取包含 _test.go 文件的目录列表
+directories=$(find . -type f -name '*_test.go' -exec dirname {} \; | sort -u)
 
 # 遍历每个子目录并执行命令
 for dir in $directories; do
 	echo -e "\e[31mExecuting command in directory: $dir\e[0m"
 	cd "$dir" || exit
 
-	go test -timeout 30s -short -v ./... || fail
+	go test -timeout 30s -short -v || fail
 
 	cd - >/dev/null || exit
 	echo -e "\n"
